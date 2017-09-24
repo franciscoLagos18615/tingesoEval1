f40e3de906577adb0d5948fa1638d3339762459d
franciscoLagos18615 <francisco.lagos.s@usach.cl>
2017-09-24 11:32:53 -0300

    add README.md and data in database

---
+++ README.md
@@ -0,0 +1,7 @@
+Trabajo numero de 1 de tingeso
+
+* Primero Crear la base de datos conn nombre estudiantes en mysqlWorkBench con el localhost:3306, por defecto de mysql
+
+* Luego en la carpeta raiz mvn spring-boot:run
+
+* Importante, recordar cambiar las credenciales de mysql en application.properti
---
+++ src/main/resources/data.sql
@@ -0,0 +1,34 @@
+INSERT INTO `estudiantes`.`student`
+(`alumno_id`,
+ `anio_ingreso`,
+ `apellido`,
+ `carrera`,
+ `email`,
+ `primer_nombre`,
+ `rut`)
+VALUES
+  (1,
+   2015,
+   'delgado',
+   'Ing.Civil en Informatica',
+   'sebastian.delgado@usach.cl',
+   'sebastian',
+   '18.888.888-8');
+
+INSERT INTO `estudiantes`.`student`
+(`alumno_id`,
+ `anio_ingreso`,
+ `apellido`,
+ `carrera`,
+ `email`,
+ `primer_nombre`,
+ `rut`)
+VALUES
+  (2,
+   2014,
+   'gonzalez',
+   'Ing.Ejecucion en computacion e informatica',
+   'marcelo.gonzalez@usach.cl',
+   'marcelo',
+   '11.111.111-1');
+
