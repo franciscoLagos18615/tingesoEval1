fe35ff45103d5beac603c4cd91cd11ce2db78941
franciscoLagos18615 <francisco.lagos.s@usach.cl>
2017-09-25 22:12:23 -0300

    add frontend with create student, modify method post

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -7,24 +7,5 @@ angular.module('librosApp', [ ])*/
 
 
 
-/*var app = angular.module('pokemon',['ngRoute']);
 
-app.config(function($routeProvider){
-    $routeProvider
-        .when('/types',{
-            templateUrl: 'js/views/pokemonType.html',
-            controller: 'PokemonTypeController'
-        })
-        .when('/alumno',{
-            templateUrl: 'js/views/pokemon.html',
-            controller: 'PokemonController'
-        })
-        .when('/movements',{
-            templateUrl: 'js/views/pokemonMovement.html',
-            controller: 'PokemonMovementController'
-        })
-        .otherwise({
-            redirectTo: '/index'
-        });
-});*/
 
